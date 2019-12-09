398220b66c02a696d09832c0343f979699f872ad
Faiz Zaman <faiz-uz.zaman@dur.ac.uk>
2019-12-09 14:09:50 +0000

    Created login/signup screen and began functionality

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/receiver")
+def validate():
+    books = {}
+    ratings = {}
 
 if __name__ == "__main__":
     app.run()
---
+++ static/js/frontend.js
@@ -0,0 +1,27 @@
+"use strict";
+
+$(document).ready(function(){
+
+    $(".card-body-signup").hide();
+    
+    $(".form-signin").on('submit', function(){
+
+        /* get ID and check if valid */
+        const userID = $("#userID").val();
+        
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
