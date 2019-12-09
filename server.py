f6fae3b0d8716c9e937563944109eaa65119791f
Faiz Zaman <faiz-uz.zaman@dur.ac.uk>
2019-12-09 16:18:38 +0000

    Added users csv file and read in files using pandas

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import csv
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -7,10 +7,14 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
 @app.route("/receiver")
 def validate():
-    books = {}
-    ratings = {}
+
+    books = pd.read_csv("dataset/books.csv")
+    ratings = pd.read_csv("dataset/ratings.csv")
+    users = pd.read_csv("dataset/users.csv")
+
 
 if __name__ == "__main__":
     app.run()
